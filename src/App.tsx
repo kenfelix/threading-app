3c6bc8cd99abd0fc7eac85ce8f783d66e58bebd2
Emeka Okafor <Emeka.Okafor@mainone.net>
2024-06-01 21:50:22 +0100

    building UI

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,35 @@ import "@twa-dev/sdk";
 import "./App.css";
 import { App as KonstaApp } from "konsta/react";
 import { isAndroid } from "react-device-detect";
+import WebApp from "@twa-dev/sdk";
 
 function App() {
   const { network } = useTonConnect();
   const theme = isAndroid ? "material" : "ios"
-
+  const username = WebApp.initData
+  console.log(username)
   return (
     <KonstaApp theme={theme} safeAreas>
-      <main className="min-h-screen p-5 bg-bg dark:bg-black">
+      <main className="min-h-screen p-5">
         <div className="max-w-[900px] mx-auto my-0 mt-10">
-          <div className="flex flex-col gap-[10px] items-center justify-center">
+          <div className="flex flex-col gap-[30px] items-center justify-center">
             <div className="flex flex-col items-center justify-center">
               <p className="text-sm font-semibold text-black dark:text-white">Telegram Name</p>
               <p className="text-xs font-semibold text-grey dark:text-gray-200">Contract Address</p>
             </div>
+            {/* Define the SVG clipPath */}
+            <svg width="0" height="0">
+              <defs>
+                <clipPath id="clip-shape" clipPathUnits="objectBoundingBox">
+                  <path d="M0.568 0.098c0-0.054-0.028-0.098-0.062-0.098h-0.349c-0.03 0-0.054 0.024-0.054 0.054v0.799c0 0.054 0.024 0.098 0.054 0.098h0.891c0.03 0 0.054-0.024 0.054-0.054v-0.587c0-0.054-0.024-0.098-0.054-0.098h-0.328c-0.034 0-0.062-0.028-0.062-0.098z"/>
+                </clipPath>
+              </defs>
+            </svg>
+
+            <div className="relative w-[366px] h-[184px]">
+              <div className="absolute top-0 left-0 w-full h-full bg-white clip-path border"></div>
+            </div>
+
             <div className="flex items-center gap-[10px]">
               <TonConnectButton />
               <button>
