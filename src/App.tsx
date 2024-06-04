a0bc5597db933265796edec6d6c40e249fc692ac
Emeka Okafor <Emeka.Okafor@mainone.net>
2024-06-04 16:17:32 +0100

    building UI

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,19 @@ function App() {
   return (
     <KonstaApp theme={theme} safeAreas>
       <main className="min-h-screen p-5">
-        <div className="max-w-[900px] mx-auto my-0 mt-10">
+        <div className="max-w-[900px] mx-auto my-0">
           <div className="flex flex-col gap-[30px] items-center justify-center">
-            <div className="bg-black w-full h-[25px] flex items-center justify-center">
-              <div className="flex items-center justify-between flex-grow max-w-[350px]">
-                <p className="text-xs text-white">All participants <span className="text-[13px] text-[#FF6ADE]">1 525 202</span></p>
-                <div className="w-1 h-1 bg-[#FF6ADE] rounded-full"></div>
-                <p className="text-xs text-white">Joined in 24 hours <span className="text-[13px] text-[#FF6ADE]">122</span></p>
+
+            {/* banner */}
+            <div className="w-full">
+              <div className="bg-black w-full h-[25px] flex items-center justify-center">
+                <div className="flex items-center justify-between flex-grow max-w-[350px]">
+                  <p className="text-xs text-white">All participants <span className="text-[13px] text-[#FF6ADE]">1 525 202</span></p>
+                  <div className="w-1 h-1 bg-[#FF6ADE] rounded-full"></div>
+                  <p className="text-xs text-white">Joined in 24 hours <span className="text-[13px] text-[#FF6ADE]">122</span></p>
+                </div>
               </div>
+              <div className="w-full h-[2px] bg-[#E8EAF6] "></div>
             </div>
             {/* <div className="flex flex-col items-center justify-center">
               <p className="text-sm font-semibold text-black dark:text-white">Telegram name</p>
