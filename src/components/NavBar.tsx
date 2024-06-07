d47209863b26d9d71ba3ec20222cb45056d773a9
Emeka Okafor <Emeka.Okafor@mainone.net>
2024-06-07 20:20:58 +0100

    building UI

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,7 @@ export function NavBar() {
     <>
       <TonConnectButton />
 
-      <Tabbar icons={true} className="left-0 bottom-0 fixed">
+      <Tabbar icons={true} labels={true} className="left-0 bottom-0 fixed">
         <TabbarLink
           active={activeTab === "tab-1"}
           onClick={() => setActiveTab("tab-1")}
@@ -20,6 +20,7 @@ export function NavBar() {
               material={<MenuIcon num={'1'}  />}
             />
           }
+          label={activeTab === "tab-1" ? <p>Home</p> : ""}
         />
         <TabbarLink
           active={activeTab === "tab-2"}
@@ -30,6 +31,7 @@ export function NavBar() {
               material={<MenuIcon num={'1'}  />}
             />
           }
+          label={activeTab === "tab-2" ? <p>Manage</p> : ""}
         />
         <TabbarLink
           active={activeTab === "tab-3"}
@@ -40,6 +42,7 @@ export function NavBar() {
               material={<MenuIcon num={'1'}  />}
             />
           }
+          label={activeTab === "tab-3" ? <p>More</p> : ""}
         />
       </Tabbar>
 
