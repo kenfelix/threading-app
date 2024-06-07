c46a46180053a925c454622ab3d4d0d26c538a90
Emeka Okafor <Emeka.Okafor@mainone.net>
2024-06-07 06:37:39 +0100

    building UI

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -14,7 +14,7 @@ export function HomePage() {
     const user = JSON.parse(JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g, '":"') + '"}', function (key, value) { return key === "" ? value : decodeURIComponent(value) }).user);
 
     const currentlevel = users?.get(Address.parse(wallet!))
-    console.log(currentlevel)
+    console.log(currentlevel, user)
     return (
         <div className="flex flex-col gap-3 items-center justify-center pt-[69px] pb-[65px] text-[#FFFFFF]">
             <div className="flex items-center justify-center">
