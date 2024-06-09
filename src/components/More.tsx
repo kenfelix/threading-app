30696bab6f6b9a675ef87dbac925a0ee804bfd71
Emeka Okafor <Emeka.Okafor@mainone.net>
2024-06-09 11:25:23 +0100

    building UI

--- src/components/More.tsx
+++ src/components/More.tsx
@@ -1,4 +1,3 @@
-// import WebApp from "@twa-dev/sdk";
 import { useThreadingContract } from "../hooks/useThreadingContract";
 import { useTonConnect } from "../hooks/useTonConnect";
 import { Address } from "ton-core";
@@ -6,7 +5,6 @@ import { getLastNonZeroIndex } from "../hooks/useUtils";
 import { CopyText } from "./CopyText";
 import { Badge, List, ListItem } from "konsta/react";
 import WebApp from "@twa-dev/sdk";
-import { MdKeyboardArrowRight } from "react-icons/md";
 
 export function MorePage() {
     const { users } = useThreadingContract();
@@ -34,17 +32,15 @@ export function MorePage() {
                 <Badge small className="!w-[66px] !h-[24px]" >level { Number(currentlevel!) }</Badge>
             </div>
 
-            <List strongIos outlineIos className="w-full">
+            <List className="w-full rounded-t-[19px] bg-[#084564]">
                 <ListItem
                     link
-                    media={<MdKeyboardArrowRight />}
                     title="How Threading Works"
                     subtitle="visit telegram community"
                     onClick={() => window.open("https://t.me/+bn_7kVHdDSNmY2Q0")}
                 />
                 <ListItem
-                    link
-                    media={<MdKeyboardArrowRight />} 
+                    link 
                     title="About Threading"
                     subtitle="visit telegram community"
                     onClick={() => window.open("https://t.me/+bn_7kVHdDSNmY2Q0")}
