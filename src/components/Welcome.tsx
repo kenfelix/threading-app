78379d7f43ef27ced99be5bac2c32e3262c923e4
Emeka Okafor <Emeka.Okafor@mainone.net>
2024-06-10 16:07:32 +0100

    building UI

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -15,7 +15,7 @@ import WebApp from "@twa-dev/sdk";
 export function WelcomePage() {
   const { connected, wallet,  } = useTonConnect();
   const { usersAddressList, sendWithdraw } = useThreadingContract();
-  const isMember = usersAddressList?.values().toString()?.includes(Address.parse(wallet!).toString({ bounceable: true, testOnly: false }));
+  const isMember = usersAddressList?.values().toString()?.includes(Address.parse(wallet!).toString({ bounceable: true, testOnly: false })) && false;
   const referer = WebApp.initDataUnsafe.start_param ? WebApp.initDataUnsafe.start_param : "EQB2GmX3ESvI-meFAtFj7PRNaBnokvepihuoAlWtIFoTgJcv";
 
   const navigate = useNavigate();
