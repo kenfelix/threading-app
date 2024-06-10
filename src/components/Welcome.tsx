736abb7fe22161d1618c0e64014b32481fc46853
Emeka Okafor <Emeka.Okafor@mainone.net>
2024-06-10 16:23:35 +0100

    building UI

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -15,9 +15,7 @@ import WebApp from "@twa-dev/sdk";
 export function WelcomePage() {
   const { connected, wallet,  } = useTonConnect();
   const { usersAddressList, sendWithdraw } = useThreadingContract();
-  const isMember = usersAddressList 
-    ? usersAddressList.values().toString().includes(Address.parse(wallet!).toString({ bounceable: true, testOnly: false })) 
-    : false;
+  const isMember = usersAddressList?.values().toString()?.includes(Address.parse(wallet!).toString({ bounceable: true, testOnly: false }));
   const referer = WebApp.initDataUnsafe.start_param ? WebApp.initDataUnsafe.start_param : "EQB2GmX3ESvI-meFAtFj7PRNaBnokvepihuoAlWtIFoTgJcv";
 
   const navigate = useNavigate();
@@ -50,7 +48,11 @@ export function WelcomePage() {
       {/* Buttons */}
 
       <div className="flex flex-col gap-[13px]">
-        <Button large rounded touchRipple className="!text-white" disabled={!connected && !isMember} colors={{ disabledBg: "bg-grey" }} onClick={() => {
+        <Button large rounded touchRipple className="!text-white" disabled={
+    !connected ||
+    (connected && isMember) ||
+    (connected && isMember === undefined)
+  } colors={{ disabledBg: "bg-grey" }} onClick={() => {
           sendWithdraw(referer);
         }}>Join now</Button>
         <Button large rounded touchRipple className="!text-white" onClick={() => window.open("https://t.me/+bn_7kVHdDSNmY2Q0")}><span><img src={Telegram} alt="" /></span>Telegram</Button>
