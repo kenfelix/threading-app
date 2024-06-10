5d921058c6ee4e51d2a17bc5eebcf80b712006cf
Emeka Okafor <Emeka.Okafor@mainone.net>
2024-06-10 11:25:12 +0100

    building UI

--- src/components/Manage.tsx
+++ src/components/Manage.tsx
@@ -69,6 +69,7 @@ export function ManagePage() {
                                         
                                 ))}
                             </List>
+                            <p>{ JSON.stringify(allReferrals)}</p>
                         </>
                     ))
                 }
